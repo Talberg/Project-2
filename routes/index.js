525d34c4c0a67fb498df3f092c260ee64d2a4138
Nick Koch <marauder30@gmail.com>
2020-02-05 14:11:38 -0700

    unfucking this

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var router = require("express").Router();
 var passport = require("passport");
 var isLoggedIn = require("../config/middleware/isLoggedIn");
 var authController = require("../controllers/authController.js");
-var apiRoutes = require("./api");
-
 // main route
 router.get("/", function (req, res) {
     res.render("index", { successMsg: res.locals.successMsg });
@@ -11,7 +9,7 @@ router.get("/", function (req, res) {
 
 // API routes (add files/routes within '/api' to run CRUD operations against your
 // database and to query external APIs)...
-router.use("/api", apiRoutes);
+// router.use("/api", apiRoutes);
 
 // auth routes
 router.get("/signup", authController.signup);
@@ -19,7 +17,7 @@ router.get("/login", authController.login);
 router.get("/logout", authController.logout);
 router.get("/home", isLoggedIn, authController.home);
 router.get('/notes', isLoggedIn,authController.notes)
-router.post('/notes', isLoggedIn , authController.createNote)
+router.post('/notes' , authController.createNote)
 
 router.post("/signup", passport.authenticate("local-signup", {
     successRedirect: "/home",
