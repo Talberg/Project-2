3bf4f66e158b157a9ceb06ee2c812631a3b3447d
Alexander Talberg <alexandermtalberg@gmail.com>
2020-02-06 16:58:16 -0700

    trying to use jaws

--- config/config.js
+++ config/config.js
@@ -10,11 +10,7 @@ module.exports = {
         "dialect": "mysql"
     },
     "production": {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
+        "use_env_variable": "JAWSDB_URL",
         "dialect": "mysql"
     }
 };
